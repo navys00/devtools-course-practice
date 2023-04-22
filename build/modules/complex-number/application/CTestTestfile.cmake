64494731f99065a1ced9b92bde2c9de6510530c5
navys00 <73278416+navys00@users.noreply.github.com>
2023-04-23 00:59:43 +0300

    8 commit

--- build/modules/complex-number/application/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/вуз - 3 курс/инструмены программирования/devtools-course-practice/modules/complex-number/application
-# Build directory: D:/вуз - 3 курс/инструмены программирования/devtools-course-practice/build/modules/complex-number/application
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/modules/complex-number/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/вуз - 3 курс/инструмены программирования/devtools-course-practice/modules/complex-number/src
-# Build directory: D:/вуз - 3 курс/инструмены программирования/devtools-course-practice/build/modules/complex-number/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
