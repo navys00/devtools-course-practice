139d953d6f37a490c84d773cc88cc9a7e7d86399
navys00 <73278416+navys00@users.noreply.github.com>
2023-04-23 13:21:46 +0300

    9 commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/вуз - 3 курс/инструмены программирования/Popov-Andrey-lab1
-# Build directory: D:/вуз - 3 курс/инструмены программирования/Popov-Andrey-lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("3rdparty/gtest")
-subdirs("modules/complex-number")
--- build/modules/complex-number/application/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/вуз - 3 курс/инструмены программирования/Popov-Andrey-lab1/modules/complex-number/application
-# Build directory: D:/вуз - 3 курс/инструмены программирования/Popov-Andrey-lab1/build/modules/complex-number/application
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
