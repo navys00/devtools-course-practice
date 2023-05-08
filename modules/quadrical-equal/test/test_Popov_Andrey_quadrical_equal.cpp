4e705f6a587edcfa0bbe8ef69982414cc5ba38ba
navys00 <73278416+navys00@users.noreply.github.com>
2023-05-09 00:24:19 +0300

    2 fix

--- modules/quadrical-equal/test/test_Popov_Andrey_quadrical_equal.cpp
+++ modules/quadrical-equal/test/test_Popov_Andrey_quadrical_equal.cpp
@@ -10,14 +10,14 @@ TEST(Popov_A_quadratic_equation, Discriminant_positive) {
   Quadrical_equal z1;
   std::string D_more_zero = z1.solution(1, 3, -4);
 
-  EXPECT_EQ(D_more_zero, "1_-4");
+  EXPECT_EQ(D_more_zero, "1.000000_-4.000000");
 }
 
 TEST(Popov_A_quadratic_equation, Discriminant_zero) {
   Quadrical_equal z1;
   std::string D_more_zero = z1.solution(1, -4, 4);
 
-  EXPECT_EQ(D_more_zero, "2");
+  EXPECT_EQ(D_more_zero, "2.000000");
 }
 
 TEST(Popov_A_quadratic_equation, Discriminant_negative) {
