acc67b533fe41baa63f2d260e1d4a5a06b7af33a
navys00 <73278416+navys00@users.noreply.github.com>
2023-05-29 18:00:00 +0300

    12 commit

--- modules/volumes/src/Volumes_3d.cpp
+++ modules/volumes/src/Volumes_3d.cpp
@@ -9,24 +9,9 @@ std::string application::operator()(int argc, char** argv) {
   if (argc == 1) {
     return help(argv[0]);
   }
-  if (argc >7) {
-    return "Text of error: arguments more than its needed";
-  }
-  arguments args;
-  if (args.operation == "Parallelepiped") {
-    result = Parallelepiped(2, 2, 2);
-  }
-  if (args.operation == "TruncatedCone")
-    result = TruncatedCone(5, 8, 8, 4.9, 4.9);
-
-  if (args.operation == "EllipticalCone") result = EllipticalCone(5, 6, 6);
-
-  return std::to_string(result);
 }
 
 std::string application::help(const std::string& an) {
   return "To calculate the volume of three-dimensional shapes";
 }
-
-bool application::validate(const std::string& str) { return true; }
 
